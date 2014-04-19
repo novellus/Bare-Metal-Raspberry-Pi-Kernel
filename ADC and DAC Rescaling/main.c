0c99d4ea295236da877972e395a70d45a28ba7f9
novellus <novellus.cato@gmail.com>
2014-04-19 14:17:07 -0500

    Added DAC output, rescaling, and bug fixes. All still untested :(. Testing will comemence hence forth, and will begin with just the ADC.

--- ADC and DAC Rescaling/main.c
+++ ADC and DAC Rescaling/main.c
@@ -8,8 +8,8 @@ unsigned int MOSI1;
 unsigned int MOSI2;
 unsigned int CS;
 
-int adcInput;
-unsigned int dacOutput;
+short adcInput;
+unsigned short dacOutput;
 volatile unsigned int i;
 
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
@@ -24,10 +24,15 @@ volatile unsigned int i;
 
 //control and range registers for AD7322 (ADC)
 #define conReg 0b1000001000010000
-#define ranReg 0b1010000000000000
+#define ranReg 0b1011000100000000
+
+
+//2.5V reference on ADC and DAC (MAX5312). ADC scales +-2.5V, and DAC scales 0->5V.
+//Linear function on data input: output=input*fctnA+fctnB; This should scale +-2V to 3V+-0.5V
+#define fctnA 0.25
+#define fctnB 3*(0xFFFF/5)
 
 int main(void) {
-
 	//Enable level 1 cache, and the branch predictor! (This gives a roughly 2x speedup, although I've been told to be careful with this.)
 	asm volatile ("MRC p15, 0, %0, c1, c0, 0" : "=r" (i));
 	i|=0x1800; 
@@ -76,34 +81,24 @@ int main(void) {
 	asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(CS));
 
 
+	asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(MOSI1)); //Clear data out line to ADC to prevent register writes.
+	dacOutput=0;
 	while(1) {
 		adcInput=0;
 
-		//Read Value from ADC into adcInput
-		asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(MOSI1)); //Clear data out line to disable accidental register writes on the ADC.
+		//Read Value from ADC into adcInput; output dacOutput to DAC.
 		asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(CS));
 		for(i=15;i>=0;i--) {
 			asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(SCLK));
+			if(dacOutput&(1<<i)) {asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(MOSI2));} //Output to DAC
+			else                 {asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(MOSI2));} //Output to DAC
 			asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(SCLK));
-			adcInput&=((((*READ)&MISO)>>10)<<i); //RBF Optimise?
+			adcInput|=((((*READ)&MISO)>>10)<<i); //RBF Optimise? //Input from ADC
 		}
 		asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(CS));
 
-		//=============================================BEGIN DEBUG====================================================//
-		// Output adcInput
-		for(i=15;i>=0;i--) {
-				asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(SCLK));
-				if(adcInput&(1<<i)) {asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(MOSI1));}
-				else                {asm volatile("str %[data], [%[reg]]" : : [reg]"r"(CLR), [data]"r"(MOSI1));}
-				asm volatile("str %[data], [%[reg]]" : : [reg]"r"(SET), [data]"r"(SCLK));
-		}
-		//=============================================End DEBUG======================================================//
-
-		adcInput=(adcInput&0x3FFE)<<2; //zero out the ZERO, address, and useless LSB; then format like a signed integer.
-
-		//Output data to the DAC (MAX5312)
-
-
+		//Right and left shift here so the processor will take care of two's complement
+		dacOutput=((unsigned short) (((int) ((adcInput&0x3FFE)<<2)*fctnA)+fctnB))>>4; //zero out the ZERO, address, and useless LSB; then format like a signed integer. Apply linear function.
 		nop2; //Delay between samples
 	}
 }
